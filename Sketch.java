e56e615170e06170efe754b2a6ba04604fd1b81f
athishasurees <athisha.surees26@ycdsbk12.ca>
2024-06-12 16:53:18 -0400

    done

--- Sketch.java
+++ Sketch.java
@@ -1,8 +1,24 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
-	
+PImage imgBackground; 
+PImage imgTornado;
+
+ float fltTornadoX = 200;
+  float fltTornadoY = 200;
+  float fltTornadoSpeed = 2; 
+  float fltTornadoDirectionX = -1; 
+  float fltTornadoDirectionY = -1; 
+
+
+  float fltSqrX = width/2;
+  float fltSqrY = width/2; 
+  float fltSqrSpeed = 5; 
+  float fltSqrDirectionX = -1; 
+  float fltSqrDirectionY = -1; 
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
@@ -16,21 +32,43 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+   imgBackground = loadImage("background.jpeg");
+   imgBackground.resize(imgBackground.width*3, imgBackground.height*3);
+   imgTornado = loadImage("tornado.png");
+   imgTornado.resize(imgTornado.width/2, imgTornado.height/2);
   }
+
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    image (imgBackground, 0, 0);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    image(imgTornado, fltTornadoX, fltTornadoY); 
+    fltTornadoX = fltTornadoX + (fltTornadoDirectionX*fltTornadoSpeed);
+    if (fltTornadoX < 0 || fltTornadoX > width - imgTornado.width){
+      fltTornadoDirectionX = fltTornadoDirectionX * -1; 
+    }
+    fltTornadoY = (float)0.02 * (float)Math.pow(fltTornadoX - 125, 2); 
+    fltTornadoY = fltTornadoY + (fltTornadoDirectionY*fltTornadoSpeed);
+    if (fltTornadoY < 0 || fltTornadoY > height - imgTornado.height){
+      fltTornadoDirectionY = fltTornadoDirectionY * -1; 
+    }
+
+    fill(255,0,0); 
+    rect(fltSqrX, fltSqrY, 50, 50);
+      fltSqrX = fltSqrX + (fltSqrDirectionX*fltSqrSpeed);
+    if (fltSqrX < 0 || fltSqrX > width - 50){
+      fltSqrDirectionX *= -1; 
+    }
+
+     fltSqrY = fltSqrY + (fltSqrDirectionY*fltSqrSpeed);
+    if (fltSqrY < 0 || fltSqrY > height - 50){
+      fltSqrDirectionY *= -1; 
+    }
+  }
   }
   
   // define other methods down here.
-}
